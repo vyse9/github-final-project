35fd9c1d4170087c2e513c474f5fee1bc4c8bc24
vyse9 <96780161+vyse9@users.noreply.github.com>
2022-09-04 23:49:17 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,4 +1,16 @@
- echo "Enter the principal:"
+ # Author: Upkar Lidder (IBM)
+   # Additional Authors:
+   # <Mohammad Sameer Hussain>
+
+   # Input:
+   # p, principal amount
+   # t, time period in years
+   # r, annual rate of interest
+
+   # Output:
+   # simple interest = p*t*r
+
+   echo "Enter the principal:"
    read p
    echo "Enter rate of interest per year:"
    read r
